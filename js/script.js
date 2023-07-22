a721de5f80aec4f4d7729f0c5fabb9daf6238ede
md nabab sharif <mdnababsharifdiu@gmail.com>
2023-07-23 01:20:54 +0600

    done all section

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+$('.main_slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+  prevArrow: '<i class="fa-solid fa-angle-left left"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right right"></i>'
+});
+
+
+
+// for banner slide 
+
+$('.banner_slide').slick({
+  arrows:false,
+  dots:true
+});
