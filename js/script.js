9ff05e99247c1c938e190c14a4f4dd0d9724810f
md nabab sharif <mdnababsharifdiu@gmail.com>
2023-07-24 00:00:18 +0600

    done responsive design

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
 
+
 $('.main_slider').slick({
+  arrows: true,
   slidesToShow: 3,
   slidesToScroll: 1,
-  arrows: true,
   prevArrow: '<i class="fa-solid fa-angle-left left"></i>',
-  nextArrow: '<i class="fa-solid fa-angle-right right"></i>'
-});
+  nextArrow: '<i class="fa-solid fa-angle-right right"></i>',
+  responsive: [
 
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
+});
 
 
 // for banner slide 
 
 $('.banner_slide').slick({
-  arrows:false,
-  dots:true
+  arrows: false,
+  dots: true
 });
